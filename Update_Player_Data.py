c0312987d0075ff7035c823a798a9803672f0806
fmathews11 <frank.mathews@handleglobal.com>
2023-12-15 12:20:43 -0500

    Implementing updated player information data frame hub

--- Update_Player_Data.py
+++ Update_Player_Data.py
@@ -6,14 +6,16 @@ import numpy as np
 import time
 from tqdm import tqdm
 from modules.objects import TeamPlayerDataframeHub
+from datetime import datetime
 
+TODAY = str(datetime.today()).split(" ")[0]
 SEASON = 2024
 
 # Pull in the team:UI JSON file
 with open('json_files/team_uis.json', 'r') as f:
     team_name_id_dict = json.load(f)
 # Create a logger
-logger = create_logger("StatusLogger", 'info')
+logger = create_logger("StatusLogger", 'debug')
 
 
 def main() -> None:
@@ -22,7 +24,7 @@ def main() -> None:
     for team_name, ui in tqdm(team_name_id_dict.items()):
 
         url = f'https://www.sports-reference.com/cbb/schools/{ui}/men/{SEASON}.html#all_roster'
-        logger.debug(team_name, url)
+        logger.debug(f"{team_name} | {url}")
         r = requests.get(url)
 
         try:
@@ -35,10 +37,15 @@ def main() -> None:
 
         hub = TeamPlayerDataframeHub(team_name=team_name.upper(),
                                      player_info=dfs[0],
-                                     player_per_game_simple=dfs[3],
-                                     player_totals=dfs[4],
-                                     player_per_100_possessions=dfs[6],
-                                     player_advanced=dfs[7])
+                                     player_per_game_simple=dfs[5],
+                                     player_per_game_simple_conf=dfs[6],
+                                     player_totals=dfs[7],
+                                     player_totals_conf=dfs[8],
+                                     player_per_100_possessions=dfs[11],
+                                     player_per_100_possessions_conf=dfs[12],
+                                     player_advanced=dfs[13],
+                                     player_advanced_conf=dfs[14])
+
         output_dict[team_name.upper()] = hub.to_dict()
         time.sleep(np.random.randint(4, 6))
 
@@ -48,6 +55,9 @@ def main() -> None:
             logger.debug(f"Sleeping for {time_to_sleep} seconds")
             time.sleep(time_to_sleep)
             stop_to_rest_counter = 0
+
+    with open(f'json_files/teams_and_players_{TODAY}.json', 'w') as f:
+        json.dump(output_dict, f)
 
     logger.info("DONE!")
 
